78a210a13892a555a06924a8077cbbcdf61e963f
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-08 11:56:51 -0700

    Use cron job instead of fs.watch

--- server/worker.js
+++ server/worker.js
@@ -1,143 +1,147 @@
 var fs = require('fs');
-// var http = require( 'http' );
-// var db = require('./database/interface.js');
-// var AlchemyAPI = require( './assets/alchemyapi');
-// var alchemyapi = new AlchemyAPI( '73ad3b222a6bcb7a40192e87eb2a393469e08fcf' );
-// var stopwords = require('./assets/stopwords.js');
+var cron = require( 'cron' ).CronJob;
+var http = require( 'http' );
+var db = require('./database/interface.js');
+var AlchemyAPI = require( './assets/alchemyapi');
+var alchemyapi = new AlchemyAPI( '73ad3b222a6bcb7a40192e87eb2a393469e08fcf' );
+var stopwords = require('./assets/stopwords.js');
 
-// fs.watch('./queue', function (event, filename) {
-//   if (filename) {
-//     if( fs.readdirSync( './queue/' ).indexOf( filename ) >= 0 ) {
-//       var data = JSON.parse( fs.readFileSync( './queue/' + filename, 'utf8' ) );
-//       fs.unlink( './queue/' + filename );
-//       alchemyapi.keywords('text', data.text, {sentiment: true}, function keywordsCallback( keywordsJSON ) {
-//         data.text = data.text.replace(/[\.\,\:\;\'\"\?\!']/g, '');
-//         if( keywordsJSON.status && keywordsJSON.status === 'ERROR' ) {
-//           throw keywordsJSON.statusInfo;
-//         } else {
-//           alchemyapi.sentiment('text', data.text, null, function documentCallback( documentJSON ) {
-//             if(documentJSON.status && documentJSON.status === 'ERROR' ) {
-//               throw documentJSON.statusInfo;
-//             }
-//             var words = [];
-//             keywordsJSON.keywords.forEach( function( keyword ) {
+new cron( '* * * * * *', function( ) {
+  var files = fs.readdirSync( './queue/' );
+  if( !( files.length > 0 ) ) {
+    return;
+  }
+  var filename = files[ 0 ];
+  for( var i = 0; i < files.length; i++ ) {
+    if( +files[ i ] < +filename ) {
+      filename = files[ i ];
+    }
+  }
 
-//               var found = false;
-//               words.forEach(function(wordToStore) {
+  var data = JSON.parse( fs.readFileSync( './queue/' + filename, 'utf8' ) );
+  fs.unlink( './queue/' + filename );
+  alchemyapi.keywords('text', data.text, {sentiment: true}, function keywordsCallback( keywordsJSON ) {
+    data.text = data.text.replace(/[\.\,\:\;\'\"\?\!']/g, '');
+    if( keywordsJSON.status && keywordsJSON.status === 'ERROR' ) {
+      throw keywordsJSON.statusInfo;
+    } else {
+      alchemyapi.sentiment('text', data.text, null, function documentCallback( documentJSON ) {
+        if(documentJSON.status && documentJSON.status === 'ERROR' ) {
+          throw documentJSON.statusInfo;
+        }
+        var words = [];
+        keywordsJSON.keywords.forEach( function( keyword ) {
 
-//                 if (wordToStore.text === keyword.text){
+          var found = false;
+          words.forEach(function(wordToStore) {
 
-//                   wordToStore.frequency += 1;
-//                   wordToStore.averageSentiment =
-//                     (wordToStore.frequency - 1) * wordToStore.averageSentiment /
-//                       wordToStore.frequency +
-//                     keyword.sentiment.score / wordToStore.frequency;
-//                   found = true;
+            if (wordToStore.text === keyword.text){
 
-//                 }
+              wordToStore.frequency += 1;
+              wordToStore.averageSentiment =
+                (wordToStore.frequency - 1) * wordToStore.averageSentiment /
+                  wordToStore.frequency +
+                keyword.sentiment.score / wordToStore.frequency;
+              found = true;
 
-//               });
-//               if (!found) {
+            }
 
-//                 words.push({
-//                   text: keyword.text,
-//                   averageSentiment: keyword.sentiment.score,
-//                   frequency: 1
-//                 });
+          });
+          if (!found) {
 
-//               }
+            words.push({
+              text: keyword.text,
+              averageSentiment: keyword.sentiment.score,
+              frequency: 1
+            });
 
-//               data.text = data.text.replace(keyword.text, '');
+          }
 
-//             });
+          data.text = data.text.replace(keyword.text, '');
 
-//             var allWords = data.text.trim().split(' ');
+        });
 
-//             allWords.forEach(function(word, index) {
-//               word.trim();
-//               if (word === '') {
-//                 allWords.splice(index, 1);
-//               }
-//             });
+        var allWords = data.text.trim().split(' ');
 
-//             allWords = allWords.filter(function(word) {
-//               if (stopwords[word] === null) {
-//                 return false;
-//               } else {
-//                 return true;
-//               }
+        allWords.forEach(function(word, index) {
+          word.trim();
+          if (word === '') {
+            allWords.splice(index, 1);
+          }
+        });
 
-//             });
+        allWords = allWords.filter(function(word) {
+          if (stopwords[word] === null) {
+            return false;
+          } else {
+            return true;
+          }
 
-//             allWords.forEach(function(word) {
-//               var found = false;
-//               words.forEach(function(wordToStore) {
-//                 if (wordToStore.text === word) {
-//                   wordToStore.frequency += 1;
-//                   found = true;
-//                 }
+        });
 
-//               });
-//               if (!found) {
-//                 words.push({
-//                   text: word,
-//                   averageSentiment: documentJSON.docSentiment.score,
-//                   frequency: 1
-//                  });
-//               }
-//             });
+        allWords.forEach(function(word) {
+          var found = false;
+          words.forEach(function(wordToStore) {
+            if (wordToStore.text === word) {
+              wordToStore.frequency += 1;
+              found = true;
+            }
 
-//             words.forEach(function(word) {
+          });
+          if (!found) {
+            words.push({
+              text: word,
+              averageSentiment: documentJSON.docSentiment.score,
+              frequency: 1
+             });
+          }
+        });
 
-//               db.Word.findOne( { where:
+        words.forEach(function(word) {
 
-//                 {
+          db.Word.findOne( { where:
 
-//                   word: word.text,
-//                   userId: data.userId,
-//                   emotion: data.emotion
+            {
 
-//                 }
-//               })
-//               .then(function(wordRecord) {
+              word: word.text,
+              userId: data.userId,
+              emotion: data.emotion
 
-//                 if (wordRecord) {
+            }
+          })
+          .then(function(wordRecord) {
 
-//                   wordRecord.increment( 'frequency', { by: word.frequency });
-//                   wordRecord.set( 'averageSentiment',
-//                     wordRecord.get( 'averageSentiment' ) * (wordRecord.get('frequency') - word.frequency) /
-//                       wordRecord.get( 'frequency' ) +
-//                     word.frequency * word.averageSentiment / wordRecord.get( 'frequency' )
+            if (wordRecord) {
 
-//                   );
-//                   wordRecord.save();
-//                 } else {
-//                   db.Word.create({
+              wordRecord.increment( 'frequency', { by: word.frequency });
+              wordRecord.set( 'averageSentiment',
+                wordRecord.get( 'averageSentiment' ) * (wordRecord.get('frequency') - word.frequency) /
+                  wordRecord.get( 'frequency' ) +
+                word.frequency * word.averageSentiment / wordRecord.get( 'frequency' )
 
-//                     word: word.text,
-//                     frequency: word.frequency,
-//                     averageSentiment: word.averageSentiment,
-//                     userId: data.userId,
-//                     emotion: data.emotion
+              );
+              wordRecord.save();
+            } else {
+              db.Word.create({
 
-//                   });
+                word: word.text,
+                frequency: word.frequency,
+                averageSentiment: word.averageSentiment,
+                userId: data.userId,
+                emotion: data.emotion
 
-//                 }
+              });
 
-//               })
-//               .catch(function(error) {
-//                 throw error;
-//               });
+            }
 
-//             });
-            
-//           });
-//         }
-//       });
-//     }
-//   }
-// });
+          })
+          .catch(function(error) {
+            throw error;
+          });
 
-fs.watch( './queue', function( event, filename ) {
-  console.log( event, filename );
-});
+        });
+        
+      });
+    }
+  });
+}, null, true, 'America/Los_Angeles');
