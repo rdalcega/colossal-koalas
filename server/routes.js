b06ecc48a494a7ae436bee3ec5f1af332ff34429
Rodrigo Delgado <rdalcega@gmail.com>
2015-10-08 12:29:08 -0700

    Added console logs

--- server/routes.js
+++ server/routes.js
@@ -155,7 +155,8 @@ var pathHandlers = {
               text: req.body.text,
               emotion: req.body.emotion
             };
-
+            console.log( 'About to write file to ' + paths.queue + Date.now( ) );
+            console.log( 'fileContent: ' + JSON.stringify( toQueue ) );
             // queue directory is used to make calls to Alchemy API on separate worker
             fs.writeFile( paths.queue + Date.now(), JSON.stringify(toQueue));
 
